4c84c05fea3ae1d0554805fbbdf5f67dd8385fbe
Nelson Morales <moralebianco@gmail.com>
2024-10-08 17:16:53 -0400

    Create util module
    The clone function is useful for parsing objects from SQL databases to JSON format or vice versa. Without the 2nd paramter it is just a shallow-clone function.

---
+++ src/util.js
@@ -0,0 +1,21 @@
+export function snakeToCamel(str) {
+  return str.replace(/([a-z]_[a-z])/g, g => `${g[0]}${g[2].toUpperCase()}`)
+}
+
+export function camelToSnake(str) {
+  return str.replace(/([a-z][A-Z])/g, g => `${g[0]}_${g[1].toLowerCase()}`)
+}
+
+/**
+ * @param {object} obj
+ * @param {(str: string) => string} func
+ * @returns {object}
+ */
+export function clone(obj, func = (s) => s) {
+  if (typeof obj != 'object')
+    return obj
+  const copy = {}
+  for (const key in obj)
+    copy[func(key)] = obj[key]
+  return copy
+}
