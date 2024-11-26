a0a10b7b4b3127cd334a9b0e41df40de5c7a7f6c
Nelson Morales <moralebianco@gmail.com>
2024-11-26 14:24:11 -0500

    Add morgan logger

--- src/index.cjs
+++ src/index.cjs
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const fs = require('node:fs')
 
 const app = express()
+
+app.use(morgan('common', {
+  skip: (_, res) => res.statusCode < 400
+}))
 
 fs.readdirSync('./src/modules').forEach(f => {
   app.use(`/${f.slice(0, -3)}s`, require(`./modules/${f}`).default)
