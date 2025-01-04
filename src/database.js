ddf70d5fe71a710fdb571ff83b04511357eebf48
Nelson Morales <moralebianco@gmail.com>
2025-01-04 15:19:37 -0500

    Add types

--- src/database.js
+++ src/database.js
@@ -1,8 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { DatabaseSync } from 'node:sqlite';
 
-if (!process.env.DB_NAME) throw new Error();
-
 const database = new DatabaseSync(process.env.DB_NAME);
 
 database.exec(readFileSync('init.sql', 'utf8'));
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DB_NAME: string;
+    APP_PORT: number;
+  }
+}
